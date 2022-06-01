879b71710c366d241fb2d4c1d14c1a98412e1d0b
Anthony Ricaud <anthony@ricaud.me>
2022-06-01 10:20:46 +0100

    Always redirect to HTTPS

--- core/settings.py
+++ core/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", cast=bool)
