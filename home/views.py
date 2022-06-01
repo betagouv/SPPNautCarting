0e26fd3ca3035f5aa90182fa68d55b2e2c5cf5e2
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-06-01 17:19:18 +0200

    Link Interface to Generator (#16)

--- home/views.py
+++ home/views.py
@@ -1,8 +1,29 @@
 """
 Views for home module
 """
-from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.http import FileResponse
+from django.views.generic import FormView
+
+from home.forms import UploadFileForm
 
 
-def index(request):
-    return render(request, "index.html")
+class Index(FormView):
+    form_class = UploadFileForm
+    template_name = "index.html"
+
+    def form_valid(self, form):
+        response = requests.post(
+            settings.GENERATOR_SERVICE_HOST, files={"file": form.cleaned_data["file"]}
+        )
+
+        http_response = FileResponse(response)
+        headers_to_forward = ["Content-Type", "Content-Length", "Content-Disposition"]
+        for header in headers_to_forward:
+            http_response.headers[header] = response.headers[header]
+
+        return http_response
+
+
+index = Index.as_view()
