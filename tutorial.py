d64a9de0309ccaa60981cff0301239ffec0808cb
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-09-08 09:57:13 +0200

    add context

--- tutorial.py
+++ tutorial.py
@@ -7,8 +7,8 @@ app.open()
 
 
 # at the bottom of the file
-@app.task(name="sum")
-def sum(a, b):
+@app.task(name="sum", retry=3, pass_context=True)
+def sum(context: procrastinate.JobContext, a, b):
     pass
     # time.sleep(random.random() * 5)  # Sleep up to 5 seconds
     # return a + b
