5a25a19c29c8ee5af16fc8c70685f193a40fa9b6
JulieMoncorger <julie.moncorger@agence3pp.com>
2023-01-26 15:21:36 +0100

    work in progress - avec les fichiers...

--- home/views.py
+++ home/views.py
@@ -132,21 +132,26 @@ def display_document_xml(request):
 
     response = generator.get(f"{settings.GENERATOR_SERVICE_HOST}/carting/c22_S1xyp/")
     document_xml = requests.get(response.text)
-    content_document_xml = document_xml.content.decode("utf-16")
-    import xml.etree.ElementTree as ET
+    content_document_xml = document_xml.content
+    import lxml.etree as ET
 
     content_root = ET.fromstring(content_document_xml)
+    content_xslt = ET.parse("home/document.xslt")
+    transform = ET.XSLT(content_xslt)
+
     paragraphe = None
     for spara in content_root.iter("sPara"):
         if spara.find("titre").find("numero").text == "4.1.1.1.":
             paragraphe = spara
+
+    transform_spara = transform(paragraphe)
 
     return render(
         request,
         "display_document_xml.html",
         {
             "presigned_url": response.text,
-            "document_xml": ET.tostring(paragraphe),
+            "document_xml": ET.tostring(transform_spara, pretty_print=True),
         },
     )
 
