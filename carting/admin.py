cc04e0d3c8facc87fa6a0f3c4aa39eebdaedcaa6
Nicolas Oudard <nicolas@oudard.org>
2023-02-02 10:56:08 +0100

    WIP

--- carting/admin.py
+++ carting/admin.py
@@ -1,8 +1,30 @@
-from django.contrib import admin
+from django.contrib.gis import admin, forms
+from django.contrib.gis.geometry import json_regex
+from django.db import models
+
+from carting.widgets import CustomOSMWidget, CustomOSMWidget2
 
 from .models import Element
 
+
 # Register your models here.
+@admin.register(Element)
+class ElementAdmin(admin.GISModelAdmin):
+    pass
 
+    # gis_widget = CustomOSMWidget2
+    gis_widget_kwargs = {
+        "attrs": {
+            "default_lat": 48.6,
+            "default_lon": -2.0,
+            "default_zoom": 10,
+            "display_raw": True,
+            "map_srid": 4326,
+        }
+    }
 
-admin.site.register(Element)
+    # formfield_overrides = {
+    #     models.TextField: {
+    #         "widget": admin.OSMWidget(attrs={"map_width": 800, "map_height": 500})
+    #     },
+    # }
--- carting/management/commands/import_in_from_xml.py
+++ carting/management/commands/import_in_from_xml.py
@@ -7,6 +7,29 @@ from django.core.management.base import BaseCommand
 from core import generator
 
 
+class INElement:
+    element: ET.Element
+
+    def __init__(self, element):
+        self.element = element
+
+    def get_bpn_id(self):
+        return (
+            self.element.attrib["bpn_id"]
+            if "bpn_id" in self.element.attrib
+            else "not found"
+        )
+
+    def get_content(self):
+        return ET.fromstring(self.element)
+
+
+class INChapterElement(INElement):
+    def get_content(self):
+        titre = self.element.find("titre")
+        return ET.tostring(titre, encoding="unicode", method="xml")
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
         # Named (optional) arguments
@@ -17,15 +40,26 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        ouvrage = options["ouvrage"]
+        def get_bpn_id(element):
+            return (
+                element.attrib["bpn_id"] if "bpn_id" in element.attrib else "not found"
+            )
 
+        ouvrage = options["ouvrage"]
         response = generator.get(
             f"{settings.GENERATOR_SERVICE_HOST}/carting/{ouvrage}/"
         )
-        print(response.text)
         document_xml = requests.get(response.text)
-        content_document_xml = document_xml.content.decode("utf-16")
-        print(content_document_xml)
-        #        content_root = ET.fromstring(content_document_xml)
-        #        print(content_root.tag)
-        pass
+        content_document_xml = document_xml.text  # .content.decode("utf-8")
+        content_root = ET.fromstring(content_document_xml)
+        ouvrage = content_root.find("ouvrage")
+        for chapter in [
+            INChapterElement(chapter) for chapter in ouvrage.iter("chapitre")
+        ]:
+
+            print(f"Chapter {chapter.get_bpn_id()}")
+            print(chapter.get_content())
+            for chapter_alinea in chapter.element.findall("alinea"):
+                print(f"Alinea {get_bpn_id(chapter_alinea)}")
+            for sub_chapter in chapter.element.iter("sChapitre"):
+                print(f"SubChapter {get_bpn_id(sub_chapter)}")
