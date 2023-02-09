8ccc3aceb8f2f62d90b6ccdf25c747fbe0e4d1b5
Anthony Ricaud <anthony@ricaud.me>
2023-02-09 16:28:16 +0000

    Another WIP session !

--- carting/models.py
+++ carting/models.py
@@ -1,4 +1,9 @@
+from functools import cached_property
+
+import lxml.etree as ET
 from django.contrib.gis.db import models
+from django.core.serializers import serialize
+from django.utils.safestring import mark_safe
 
 
 class ElementTypology(models.TextChoices):
@@ -15,7 +20,6 @@ class ElementTypology(models.TextChoices):
     ILLUSTRATION = "ILLUSTRATION", "illustration"
 
 
-# Create your models here.
 class Element(models.Model):
 
     bpn_id = models.CharField(max_length=40, primary_key=True)
@@ -30,7 +34,25 @@ class Element(models.Model):
     )
     xpath = models.CharField(max_length=512)
     ouvrage_name = models.CharField(max_length=10)
+    # FIXME: Valider la géométrie ?
+    # geos.geometry.GEOSGeometry.valid
     geometry = models.GeometryField(null=True, blank=True, default=None, srid=4326)
 
     def __str__(self):
         return f"{self.numero} - {self.bpn_id} - {self.typology} - {self.xpath}"
+
+    def json(self):
+        return serialize("geojson", [self], fields=("geometry",))
+
+    @cached_property
+    def content_html(self):
+        print("Coucou julie")
+        if not self.content:
+            return ""
+
+        content_root = ET.fromstring(self.content)
+        content_xslt = ET.parse("carting/document.xslt")
+        transform = ET.XSLT(content_xslt)
+        transform_content = transform(content_root)
+
+        return mark_safe(transform_content)
