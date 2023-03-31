0594b06a3bd910db98ceac81e8fbd2455590f851
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-03-31 12:02:48 +0200

    Update unit_tests/spo/test_forms.py

--- unit_tests/spo/test_forms.py
+++ unit_tests/spo/test_forms.py
@@ -13,16 +13,6 @@ class TestUploadFileForm:
         assert form.errors == {}
         assert form.is_valid()
 
-    def test_upload_conte_form_content_type_xml_file_ko(self):
-        wrong_xml_file = SimpleUploadedFile(
-            "file.xml", b"fake", content_type="something/else"
-        )
-        form = UploadFileForm({}, {"file": wrong_xml_file})
-        assert form.errors["file"] == [
-            "Le fichier doit être un fichier tableau type format XML"
-        ]
-        assert not form.is_valid()
-
     def test_upload_conte_form_extension_xml_file_ko(self):
         wrong_xml_file = SimpleUploadedFile(
             "file.ods", b"fake", content_type="text/xml"
