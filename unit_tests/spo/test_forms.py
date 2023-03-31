3c0d313958cd645b4bdb3113f7ee207d4c243c00
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-03-31 12:11:56 +0200

    Only allow XML tables (#186)
    fix https://github.com/betagouv/SPPNautInterface/issues/87
    
    ---------
    
    Co-authored-by: Anthony Ricaud <anthony@ricaud.me>

---
+++ unit_tests/spo/test_forms.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from spo.forms import UploadFileForm
+
+
+class TestUploadFileForm:
+    def test_xml_file_ok(self):
+        xml_file = SimpleUploadedFile("file.xml", b"fake", content_type="text/xml")
+        form = UploadFileForm({}, {"file": xml_file})
+        assert form.errors == {}
+        assert form.is_valid()
+
+    def test_ods_file_refused(self):
+        wrong_xml_file = SimpleUploadedFile(
+            "file.ods", b"fake", content_type="text/xml"
+        )
+        form = UploadFileForm({}, {"file": wrong_xml_file})
+        assert form.errors["file"] == [
+            "Le fichier doit être un fichier tableau type format XML"
+        ]
+        assert not form.is_valid()
