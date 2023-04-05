518a8dcf1b5f4cce73f99d07f6fa7640c96fe503
Anthony Ricaud <anthony@ricaud.me>
2023-04-05 11:40:01 +0100

    Install debug toolbar in dev environments
    Set [`SHOW_TOOLBAR_CALLBACK`] to always display the toolbar.
    
    Use `settings.DEBUG` for BrowserReloadMiddleware as well to avoid spreading those settings in separate `if` statements.
    
    [`SHOW_TOOLBAR_CALLBACK`]: https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#show-toolbar-callback

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 import pytest
 
 
@@ -6,3 +8,9 @@ def disable_whitenoise(settings):
     settings.STATICFILES_STORAGE = (
         "django.contrib.staticfiles.storage.StaticFilesStorage"
     )
+
+
+@pytest.fixture(autouse=True)
+def disable_debug_toolbar(settings):
+    with suppress(ValueError):
+        settings.MIDDLEWARE.remove("debug_toolbar.middleware.DebugToolbarMiddleware")
