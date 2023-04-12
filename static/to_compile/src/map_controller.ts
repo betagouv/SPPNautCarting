46713d7fbccd44aa790b3d03186af8b14c0dac0f
Anthony Ricaud <anthony@ricaud.me>
2023-04-12 14:10:37 +0100

    Improve several UX aspects of our map (#210)
    Features:
    - Add fullscreen button.
    - Display coordinates at mouse position.
    - Display a scale bar.
    - Highlight the section in map when hovering the button to zoom on it.
    - Enable =/-/left/right/top/down keyboard shortcuts on the whole document.
    - Adjust geometry colors.
    
    Tech-changes:
    - Convert all our JS into Typescript and make some type adjustments. This is mostly to get some code editor help since we're not verifying the types in our build system.
    - Factor the way we dispatch OpenLayers events to browser events in `#dispatch`.
    - Configure Tailwind to discover the classes used by MousePosition

--- static/to_compile/src/map_controller.ts
+++ static/to_compile/src/map_controller.ts
@@ -1,23 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
+import type { default as PopinController } from "./popin_controller"
+
+import { Coordinate } from "ol/coordinate"
 import { SectionMap } from "./section_map"
 
-export default class extends Controller {
+export default class extends Controller<HTMLElement> {
     #map
 
     static targets = ["map", "geojson", "featurePopin"]
+    readonly mapTarget: HTMLElement
+    readonly geojsonTarget: HTMLScriptElement
+    readonly featurePopinTarget: HTMLElement
+
     static values = {
         initialCenter: { type: Array, default: [-2.0, 48.65] },
         maxZoom: { type: Number, default: 13 },
         wmsUrl: String,
     }
+    readonly initialCenterValue: Coordinate
+    readonly maxZoomValue: number
+    readonly wmsUrlValue: string
 
     connect() {
         this.#map = new SectionMap({
             target: this.mapTarget,
             initialCenter: this.initialCenterValue,
             maxZoom: this.maxZoomValue,
-            geojson: this.geojsonTarget.textContent,
+            geojson: this.geojsonTarget.textContent!,
             wmsUrl: this.wmsUrlValue,
+            fullscreenElement: this.element,
         })
 
         this.#map.fitViewToAllSections()
@@ -36,6 +47,14 @@ export default class extends Controller {
         this.#map.selectSection(bpnID)
     }
 
+    highlightSectionInMap(e) {
+        this.#map.highlightSection(e.target.getAttribute("href").replace("#", ""))
+    }
+
+    unhighlightSectionInMap(e) {
+        this.#map.unhighlightSection()
+    }
+
     showFeaturePopin(event) {
         this.#popinController.show(event.detail.html)
     }
@@ -48,6 +67,6 @@ export default class extends Controller {
         return this.application.getControllerForElementAndIdentifier(
             this.featurePopinTarget,
             "popin",
-        )
+        ) as PopinController
     }
 }
