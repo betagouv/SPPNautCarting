fb65864f5afb09bb7f6a868045ae4422314c3397
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-06-16 17:14:07 +0200

    Remove SPO dependency for ingester script

--- carting/management/commands/ingest_ouvrage_xml.py
+++ carting/management/commands/ingest_ouvrage_xml.py
@@ -1,27 +1,29 @@
 from xml.etree import ElementTree
+from pathlib import Path
 
 import requests
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from carting.models import OuvrageSection
-from spo import generator
+
+# from spo import generator
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "ouvrage",
-            help="Ouvrage name, ex = 12, c22, l1",
+            help="Ouvrage xml file, ex = ./12.xml, ./c22.xml, ./l1.xml",
         )
 
     def handle(self, *args, **options):
-        ouvrage_name = options.get("ouvrage")
-        response = generator.get(
-            f"{settings.GENERATOR_SERVICE_HOST}/url-for/{ouvrage_name}/xml/document.xml"
-        )
-        document_xml = requests.get(response.text)
-        content_root = ElementTree.fromstring(document_xml.text)
+        ouvrage_location = options.get("ouvrage")
+
+        document_path = Path(str(ouvrage_location))
+        content_root = ElementTree.parse(document_path)
         # FIXME: Valider bpn_id uniques
-        ingested = OuvrageSection.objects.ingest_xml_subtree(ouvrage_name, content_root)
+        ingested = OuvrageSection.objects.ingest_xml_subtree(
+            document_path.name[:10], content_root
+        )
         self.stdout.write(self.style.SUCCESS(f"✨ {ingested} sections ingérées"))
