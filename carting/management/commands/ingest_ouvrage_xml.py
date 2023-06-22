c0c555b8a201c1820dd55b6ccd55eca406150fac
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-06-22 09:41:48 +0200

    Ingest XML from a file instead of calling SPO (#314)
    * Remove SPO dependency for ingester script
    * XML ingester now takes an xml file path as input

--- carting/management/commands/ingest_ouvrage_xml.py
+++ carting/management/commands/ingest_ouvrage_xml.py
@@ -1,27 +1,27 @@
 from xml.etree import ElementTree
+from pathlib import Path
 
 import requests
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from carting.models import OuvrageSection
-from spo import generator
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "ouvrage",
-            help="Ouvrage name, ex = 12, c22, l1",
+            help="Ouvrage xml file, ex = ./12.xml, ./c22.xml, ./l1.xml",
+            type=Path,
         )
 
     def handle(self, *args, **options):
-        ouvrage_name = options.get("ouvrage")
-        response = generator.get(
-            f"{settings.GENERATOR_SERVICE_HOST}/url-for/{ouvrage_name}/xml/document.xml"
-        )
-        document_xml = requests.get(response.text)
-        content_root = ElementTree.fromstring(document_xml.text)
+        document_path = options.get("ouvrage")
+
+        content_root = ElementTree.parse(document_path)
         # FIXME: Valider bpn_id uniques
-        ingested = OuvrageSection.objects.ingest_xml_subtree(ouvrage_name, content_root)
+        ingested = OuvrageSection.objects.ingest_xml_subtree(
+            document_path.stem, content_root
+        )
         self.stdout.write(self.style.SUCCESS(f"✨ {ingested} sections ingérées"))
